e6b3bda628e831957c9eaa47197956c64488730a
Karol <kstecyk@o2.pl>
2026-01-26 21:13:53 +0100

    ScriptAnalyzer

---
+++ formatter.ps1
@@ -0,0 +1,8 @@
+$path = ".\test.ps1"
+
+$content = Get-Content $path -Raw
+$formatted = Invoke-Formatter -ScriptDefinition $content
+
+Set-Content -Path $path -Value $formatted -Encoding UTF8
+
+Invoke-ScriptAnalyzer -Path $path
